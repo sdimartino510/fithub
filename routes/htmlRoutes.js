e413b9631f2793d650a537606be8e1b2288729e8
Sergio Di Martino <sergio@webdevserg.io>
2019-03-30 19:14:18 -0700

    Adds routing for home and add html files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,14 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/html/home.html"));
+  });
+
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/add.html"));
   });
 
   // Load example page and pass in an example by id
