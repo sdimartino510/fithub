2edc9f522aa10254d781b1d72ae72235174be31a
Lauren Bullen <laurenbullen@comcast.net>
2019-03-31 17:14:56 -0700

    initial schema

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,17 @@ CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+
+DROP DATABASE IF EXISTS fithub_db;
+CREATE DATABASE fithub_db;
+
+CREATE TABlE profile(
+    id INTEGER(100) AUTO_INCREMENT NOT NULL,
+    name VARCHAR (100) NOT NULL,
+    gender VARCHAR (10) NOT NULL,
+    age INTEGER(10) NOT NULL,
+    weight DECIMAL(10) NOT NULL,
+    height DECIMAL(10) NOT NULL,
+    PRIMARY KEY(id)
+);
