ec46cdb6e3935514a7bb6639fee59087fa75ea04
baldricwat <40972861+baldricwat@users.noreply.github.com>
2019-04-02 20:35:31 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -12,10 +12,13 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    id: sequelize.INTEGER,
+    name: sequelize.STRING,
+    gender: sequelize.STRING,
+    age: sequelize.INTEGER,
+    weight: sequelize.INTEGER,
+    height: sequelize.INTEGER
+
   );
 }
 
